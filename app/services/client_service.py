8361d80b4eb4833146035408f93c64ea4c1cc393
Wemerson Jesus <wejesuss@gmail.com>
2025-10-29 22:11:58 -0300

    feature: add service layer for client implementing models business logic

---
+++ app/services/client_service.py
@@ -0,0 +1,31 @@
+from typing import List
+from models.client import Client
+import repositories.client_repository as client_repository
+
+def get_client_by_id(client_id: int) -> Client | None:
+    return client_repository.get_client_by_id(client_id)
+
+def get_clients(limit: int = None, offset: int = 0) -> List[Client]:
+    clients = client_repository.get_clients(limit, offset)
+    if not clients:
+        return []
+    
+    return clients
+
+def create_client(data: dict) -> Client:
+    # TODO - validate fields
+    return client_repository.insert_client(data)
+
+def update_client(client_id: int, data: dict) -> Client | None:
+    client_exists = client_repository.get_client_by_id(client_id)
+    if not client_exists:
+        return None
+
+    client = client_repository.update_client(client_id, data)
+    return client
+
+def delete_client(client_id: int) -> bool:
+    """Deactivate (soft delete) a client."""
+    success = client_repository.delete_client(client_id)
+    
+    return success
