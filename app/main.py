6a906a42fbdaa5a12db0a9f2af811bcaf7e7e471
Wemerson Jesus <wejesuss@gmail.com>
2025-10-31 11:20:41 -0300

    server: add purchase route in server application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, APIRouter
-from routes import clients
+from routes import clients, purchases
 from database import init_database
 
 app = FastAPI()
 
 app.include_router(clients.router)
+app.include_router(purchases.router)
 
 @app.on_event("startup")
 def startup_event():
