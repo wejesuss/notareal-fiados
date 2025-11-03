c776de71bf93e3e109e71806eca2f14f462e78f3
Wemerson Jesus <wejesuss@gmail.com>
2025-11-03 19:07:46 -0300

    server: add payment route in server application

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, APIRouter
-from routes import clients, purchases
+from routes import clients, purchases, payments
 from database import init_database
 
 app = FastAPI()
 
 app.include_router(clients.router)
 app.include_router(purchases.router)
+app.include_router(payments.router)
 
 @app.on_event("startup")
 def startup_event():
