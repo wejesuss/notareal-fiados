0a12877eb4a34289929a5b8bb18c2a7a9b9e6415
Wemerson Jesus <wejesuss@gmail.com>
2025-12-19 15:40:19 -0300

    feat: add basic toolbar menu navigation layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,15 +2,55 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> AgroReal </q-toolbar-title>
+        <q-btn 
+          flat 
+          dense 
+          round 
+          icon="menu" aria-label="Menu" 
+          @click="toggleDrawer" />
 
-        <div class="row items-center q-gutter-sm">
-          <q-btn flat to="/" label="Dashboard" />
-        </div>
-
+        <q-space />
+        <q-toolbar-title> NotaReal Fiados </q-toolbar-title>
         <q-space />
       </q-toolbar>
     </q-header>
+
+    <!-- DRAWER / MENU -->
+     <q-drawer 
+        v-model="leftDrawerOpen"
+        show-if-above
+        bordered
+     >
+        <q-list>
+          <q-item clickable v-ripple to="/">
+            <q-item-section avatar>
+              <q-icon name="dashboard"></q-icon>
+            </q-item-section>
+            <q-item-section>
+              Painel
+            </q-item-section>
+          </q-item>
+          
+          <q-item clickable v-ripple to="/clients">
+            <q-item-section avatar>
+              <q-icon name="people"></q-icon>
+            </q-item-section>
+            <q-item-section>
+              Clientes
+            </q-item-section>
+          </q-item>
+          
+          <q-item clickable v-ripple to="/purchases">
+            <q-item-section avatar>
+              <q-icon name="receipt_long"></q-icon>
+            </q-item-section>
+            <q-item-section>
+              Compras
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+     </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -18,4 +58,12 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+
+function toggleDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
