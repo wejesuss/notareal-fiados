7d6de89dae794aed3b961b7be5ce600840631fad
Wemerson Jesus <wejesuss@gmail.com>
2025-12-25 20:42:22 -0300

    feat: create mock functions to get client data

---
+++ frontend/src/services/client.ts
@@ -0,0 +1,30 @@
+import type { Client } from "src/models";
+
+const clients: Client[] = [
+  {
+    id: 1,
+    name: "José Augusto",
+    nickname: null,
+    email: "joseaugusto32@example.com",
+    isActive: false,
+    createdAt: "2025-12-15T22:36:31",
+    updatedAt: "2025-12-15T22:36:31",
+  },
+  {
+    id: 2,
+    name: "João Carlos",
+    nickname: "joaocarlos",
+    phone: "(99) 9900-9879",
+    isActive: true,
+    createdAt: "2025-12-15T22:36:31",
+    updatedAt: "2025-12-15T22:36:31",
+  },
+];
+
+export function getClients(): Client[] {
+  return clients;
+}
+
+export function getRecentClients(limit: number = 5): Client[] {
+  return clients.slice(0, limit);
+}
