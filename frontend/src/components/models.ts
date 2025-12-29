e45f62b68a672ad5b2dfc5dfb52c9416e07f69a4
Wemerson Jesus <wejesuss@gmail.com>
2025-12-29 18:21:05 -0300

    refactor: extract SystemOverview component props to components/models.ts file

--- frontend/src/components/models.ts
+++ frontend/src/components/models.ts
@@ -1,5 +1,28 @@
 import type { QIconProps } from "quasar";
 
+export interface SystemOverviewCardProps {
+  /**
+   * If system status is up or down
+   */
+  isHealthy: boolean;
+  /**
+   * string representing the last update time
+   */
+  lastUpdated?: string | null;
+  /**
+   * Number of `clients` currently active
+   */
+  activeClients: number;
+  /**
+   * Number of `purchases` not entirely paid (`pending` or `partial`)
+   */
+  openPurchases: number;
+  /**
+   * Total amount to receive considering not paid `purchases`
+   */
+  openAmount: number;
+}
+
 export interface RegistryCardProps {
   id: string | number;
   title: string;
