30ce49bb9e3884658bcd26fa5981a1952588461b
Wemerson Jesus <wejesuss@gmail.com>
2026-01-26 19:26:28 -0300

    feat: render "all shown" message even if pagination is active when list end is reached

--- frontend/src/pages/clients/ClientPurchasesPage.vue
+++ frontend/src/pages/clients/ClientPurchasesPage.vue
@@ -145,7 +145,7 @@
         >
         </q-pagination>
 
-        <div v-else class="text-center q-pb-sm">
+        <div v-if="page === totalPages" class="text-center q-pb-sm">
           <span class="text-caption text-grey-7 letter-spaced"
             >Todos os registros exibidos</span
           >
