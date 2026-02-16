751b3cf75d66763b33af799dd9d49fd21ab02066
Wemerson Jesus <wejesuss@gmail.com>
2026-02-16 15:12:04 -0300

    chore: remove unnecessary debugging token logging

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -10,7 +10,6 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  console.log(token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
