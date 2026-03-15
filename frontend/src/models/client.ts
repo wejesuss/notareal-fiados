c024309fee9a0fe43ca83d479ad48a2fe24fa9cf
Wemerson Jesus <wejesuss@gmail.com>
2026-03-15 16:36:15 -0300

    fix: make Client properties required on frontend model
    Keep consistency between frontend models removing absence of client optional fields (? sign) keeping them null if no value was provided.
    This ensures client always has that property defined, even if with a null value.

--- frontend/src/models/client.ts
+++ frontend/src/models/client.ts
@@ -2,9 +2,9 @@
 export interface Client {
   id: number;
   name: string;
-  nickname?: string | null;
-  phone?: string | null;
-  email?: string | null;
+  nickname: string | null;
+  phone: string | null;
+  email: string | null;
   isActive: boolean;
   createdAt: string; // ISO date
   updatedAt: string; // ISO date
