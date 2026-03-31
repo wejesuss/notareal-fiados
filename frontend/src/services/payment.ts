7260154489b57689517f3a90847997654d5f2814
Wemerson Jesus <wejesuss@gmail.com>
2026-03-30 22:55:32 -0300

    feat: integrate getPayments service function with backend endpoint

--- frontend/src/services/payment.ts
+++ frontend/src/services/payment.ts
@@ -1,30 +1,11 @@
-import type {
-  Payment,
-  PaymentListParams,
-  PaymentListResponse,
-} from "src/models";
+import type { PaymentListParams, PaymentListResponse } from "src/models";
 import { api } from "src/api/api";
-import { sleep } from "src/utils/timing/sleep";
 
-const payments: Payment[] = [
-  {
-    id: 1,
-    purchaseId: 2,
-    description: "Pagamento adiantado",
-    paymentDate: "2026-02-03T19:36:37.000Z",
-    amountCents: 4990,
-    method: "Pix",
-    receiptNumber: "REC-002-001",
-    isActive: true,
-    createdAt: "2026-02-03T19:36:37.000Z",
-    updatedAt: "2026-02-03T19:36:37.000Z",
-    amount: 49.9,
-  },
-];
+export async function getPayments(): Promise<PaymentListResponse> {
+  // Zero (0) means from all purchases
+  const purchaseId = 0;
 
-export async function getPayments() {
-  await sleep(300);
-  return payments;
+  return await getPurchasePayments(purchaseId);
 }
 
 export async function getPurchasePayments(
