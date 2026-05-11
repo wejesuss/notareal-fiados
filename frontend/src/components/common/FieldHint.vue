a5b0dec16576ef8c282206cab6e713a557f227ea
Wemerson Jesus <wejesuss@gmail.com>
2026-05-11 14:24:04 -0300

    feat: create reusable FieldHint component for form input tooltips

---
+++ frontend/src/components/common/FieldHint.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-icon :name="icon" size="xs" :class="{ 'cursor-pointer': clickable }">
+    <q-tooltip
+      anchor="top middle"
+      self="bottom middle"
+      :delay="tooltipDelay.delay"
+      :hide-delay="tooltipDelay.hideDelay ?? 0"
+      class="tooltip-medium"
+      >{{ tooltip }}</q-tooltip
+    >
+  </q-icon>
+</template>
+
+<script setup lang="ts">
+interface FieldHint {
+  icon: string;
+  tooltip: string;
+  tooltipDelay?: { delay: number; hideDelay?: number };
+  clickable?: boolean;
+}
+
+withDefaults(defineProps<FieldHint>(), {
+  clickable: false,
+  tooltipDelay: () => ({ delay: 250, hideDelay: 150 }),
+});
+</script>
