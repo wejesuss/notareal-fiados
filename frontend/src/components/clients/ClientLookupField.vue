76a447c31955417fb7ec92bf80e4b5f0d9eaa337
Wemerson Jesus <wejesuss@gmail.com>
2026-06-01 13:23:02 -0300

    refactor: optimize client lookup option reactivity

--- frontend/src/components/clients/ClientLookupField.vue
+++ frontend/src/components/clients/ClientLookupField.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, shallowRef, watch } from "vue";
 import { type QSelect, type QSelectProps } from "quasar";
 import { getClients } from "src/services";
 
@@ -38,23 +38,25 @@ type ClientLookupOption = {
 
 const props = defineProps<{ clientId: number | undefined }>();
 
-let clientOptions: ClientLookupOption[] = [];
+const clientOptions = shallowRef<ClientLookupOption[]>([]);
+const options = shallowRef<ClientLookupOption[]>([]);
+const selectedClient = ref<ClientLookupOption>();
 
 watch(
   () => props.clientId,
   async (id) => {
-    await searchClients();
+    if (clientOptions.value.length === 0) {
+      await searchClients();
+    }
+
     if (id) {
-      selectedClient.value = clientOptions.find(
+      selectedClient.value = clientOptions.value.find(
         (client) => client.value === id,
       );
     }
   },
   { immediate: true },
 );
-
-const selectedClient = ref<ClientLookupOption>();
-const options = ref(clientOptions);
 
 const filterFn: QSelectProps["onFilter"] = (
   val: string,
@@ -66,10 +68,10 @@ const filterFn: QSelectProps["onFilter"] = (
     update(
       () => {
         if (val === "") {
-          options.value = clientOptions;
+          options.value = clientOptions.value;
         } else {
           const needle = val.toLowerCase();
-          options.value = clientOptions.filter(
+          options.value = clientOptions.value.filter(
             (v) => v.label.toLowerCase().indexOf(needle) > -1,
           );
         }
@@ -92,7 +94,7 @@ function abortFilterFn() {
 async function searchClients() {
   try {
     const { clients } = await getClients({ onlyActive: false });
-    clientOptions = clients.reduce((acc, client) => {
+    clientOptions.value = clients.reduce((acc, client) => {
       acc.push({
         label: client.name,
         value: client.id,
